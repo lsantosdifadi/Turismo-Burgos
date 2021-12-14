4e38719b2cf91f31daa0942f991bd217c13eae54
Difadi <soporte@difadi.com>
2021-12-14 16:59:44 +0100

    Añadido el formulario

---
+++ contacto.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contacta con nosotros</title>
+</head>
+<body>
+    <h1>Contacta con nosotros</h1>
+
+<div id="formulario">
+
+    <form action=""></form>
+
+</div>
+<footer>Pie de pagina teléfono aviso legal </footer>
+
+</body>
+</html>
